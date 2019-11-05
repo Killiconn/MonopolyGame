e761c3550e157ef9dcb2afef9a69772268d078d3
Ethan Sharkey <ethan.sharkey3@mail.dcu.ie>
2019-11-05 17:50:19 +0000

    Trying to learn how to use git!

--- code/Property.java
+++ code/Property.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+//weird
 public class Property extends Tile
 {
     String name;
--- code/PropertyGroup.java
+++ code/PropertyGroup.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+//weird
 public class PropertyGroup extends Property
 {
 	String colour;
